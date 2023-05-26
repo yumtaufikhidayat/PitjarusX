42506c735f0fbe227cbed58420542392926a1b4a
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-05-26 13:47:50 +0700

    Implement store list.

--- app/src/main/java/com/yumtaufikhidayat/pitjarusx/ui/store/storelist/fragment/StoreListFragment.kt
+++ app/src/main/java/com/yumtaufikhidayat/pitjarusx/ui/store/storelist/fragment/StoreListFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -16,6 +18,7 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.yumtaufikhidayat.pitjarusx.R
 import com.yumtaufikhidayat.pitjarusx.databinding.FragmentStoreListBinding
 import com.yumtaufikhidayat.pitjarusx.model.login.Store
+import com.yumtaufikhidayat.pitjarusx.ui.store.storelist.adapter.StoreListAdapter
 import com.yumtaufikhidayat.pitjarusx.ui.store.storelist.viewmodel.StoreListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -28,6 +31,7 @@ class StoreListFragment : Fragment(), OnMapReadyCallback {
 
     private var mMap: GoogleMap? = null
     private val viewModel: StoreListViewModel by viewModels()
+    private val storeListAdapter by lazy { StoreListAdapter(::navigateToDetailStore) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,6 +46,7 @@ class StoreListFragment : Fragment(), OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState)
         handleBackNavigation()
         setMap()
+        setStoreListAdapter()
     }
 
     private fun handleBackNavigation() {
@@ -55,9 +60,12 @@ class StoreListFragment : Fragment(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun setStoreListAdapter() {
+        binding.rvStoreList.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = storeListAdapter
+        }
     }
 
     override fun onMapReady(map: GoogleMap) {
@@ -68,14 +76,15 @@ class StoreListFragment : Fragment(), OnMapReadyCallback {
     private fun setObserver() {
         viewModel.getLogin().observe(viewLifecycleOwner) {
             setMarkers(it)
+            showStoreList(it)
         }
     }
 
-    private fun setMarkers(store: List<Store>) {
+    private fun setMarkers(storeList: List<Store>) {
         binding.apply {
-            val username = store[0].accountName
+            val username = storeList[0].accountName
             tvToolbarSubtitle.text = username
-            store.forEach {
+            storeList.forEach {
                 val latitude = it.latitude.toDouble()
                 val longitude = it.longitude.toDouble()
                 val latLng = LatLng(latitude, longitude)
@@ -95,6 +104,19 @@ class StoreListFragment : Fragment(), OnMapReadyCallback {
                 }
             }
         }
+    }
+
+    private fun showStoreList(storeList: List<Store>) {
+        storeListAdapter.submitList(storeList)
+    }
+
+    private fun navigateToDetailStore() {
+        Toast.makeText(requireContext(), "Go to detail store", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {

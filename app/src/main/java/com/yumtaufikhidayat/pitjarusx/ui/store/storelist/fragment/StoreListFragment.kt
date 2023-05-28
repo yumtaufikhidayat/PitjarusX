2ba69452c6d482cbc72f3b2c47faa4948b2aa6f7
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-05-28 07:07:18 +0700

    Update place icon and adjust map settings.

--- app/src/main/java/com/yumtaufikhidayat/pitjarusx/ui/store/storelist/fragment/StoreListFragment.kt
+++ app/src/main/java/com/yumtaufikhidayat/pitjarusx/ui/store/storelist/fragment/StoreListFragment.kt
@@ -1,10 +1,18 @@
 package com.yumtaufikhidayat.pitjarusx.ui.store.storelist.fragment
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.ColorInt
+import androidx.annotation.DrawableRes
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -13,6 +21,8 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.yumtaufikhidayat.pitjarusx.R
@@ -70,6 +80,15 @@ class StoreListFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(map: GoogleMap) {
         mMap = map
+        map.uiSettings.apply {
+            isZoomControlsEnabled = true
+            isIndoorLevelPickerEnabled = true
+            isCompassEnabled = false
+            isMapToolbarEnabled = true
+            isScrollGesturesEnabledDuringRotateOrZoom = false
+            isRotateGesturesEnabled = false
+        }
+
         setObserver()
     }
 
@@ -94,6 +113,12 @@ class StoreListFragment : Fragment(), OnMapReadyCallback {
                         MarkerOptions()
                             .position(latLng)
                             .title(it.storeName)
+                            .icon(
+                                vectorToBitmap(
+                                    R.drawable.baseline_place,
+                                    Color.parseColor("#00FF00")
+                                )
+                            )
                     )
                     gMap.moveCamera(
                         CameraUpdateFactory.newLatLngZoom(
@@ -112,6 +137,24 @@ class StoreListFragment : Fragment(), OnMapReadyCallback {
 
     private fun navigateToDetailStore() {
         Toast.makeText(requireContext(), "Go to detail store", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun vectorToBitmap(@DrawableRes id: Int, @ColorInt color: Int): BitmapDescriptor {
+        val vectorDrawable = ResourcesCompat.getDrawable(resources, id, null)
+        if (vectorDrawable == null) {
+            Log.e("BitmapHelper", "Resource not found")
+            return BitmapDescriptorFactory.defaultMarker()
+        }
+        val bitmap = Bitmap.createBitmap(
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
+        DrawableCompat.setTint(vectorDrawable, color)
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 
     override fun onDestroyView() {
